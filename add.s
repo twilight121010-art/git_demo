8ec56aa51ee66d92d3eb427f09d648fb76bf236e
twilight <202512063005@mails.cuc.edu.cn>
2026-05-19 19:16:54 +0800

    这个分支用来存code

---
+++ add.s
@@ -0,0 +1,31 @@
+	.file	"add.c"
+	.text
+	.globl	add
+	.type	add, @function
+add:
+.LFB0:
+	.cfi_startproc
+	endbr64
+	leal	(%rdi,%rsi), %eax
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	add, .-add
+	.ident	"GCC: (Ubuntu 13.3.0-6ubuntu2~24.04) 13.3.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	1f - 0f
+	.long	4f - 1f
+	.long	5
+0:
+	.string	"GNU"
+1:
+	.align 8
+	.long	0xc0000002
+	.long	3f - 2f
+2:
+	.long	0x3
+3:
+	.align 8
+4:
